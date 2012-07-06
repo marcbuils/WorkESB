3a0e78fd10204fd154bbd7b754c3b2b1b9ef7956
Marc Buils <marc.buils@gmail.com>
2012-07-06 08:26:44 +0200

    Tool name changed in commentaries

--- messenger_gateway_websockets/messenger_gateway_websockets/src/Log.cpp
+++ messenger_gateway_websockets/messenger_gateway_websockets/src/Log.cpp
@@ -1,18 +1,18 @@
 /*
- * This file is part of M2Bench.
+ * This file is part of WorkESB.
  *
- *  M2Bench is free software: you can redistribute it and/or modify
+ *  WorkESB is free software: you can redistribute it and/or modify
  *  it under the terms of the GNU Lesser General Public License as published by
  *  the Free Software Foundation, either version 3 of the License, or
  *  (at your option) any later version.
  *
- *  M2Bench is distributed in the hope that it will be useful,
+ *  WorkESB is distributed in the hope that it will be useful,
  *  but WITHOUT ANY WARRANTY; without even the implied warranty of
  *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  *  GNU Lesser General Public License for more details.
  *
  *  You should have received a copy of the GNU Lesser General Public License
- *  along with M2Bench.  If not, see <http://www.gnu.org/licenses/>.
+ *  along with WorkESB.  If not, see <http://www.gnu.org/licenses/>.
  *
  *  Created on: 25 fevr. 2011
  *      Author: Buils
